0c6e0ba9a8ac1bd7613f92f4089850cb0519a2a6
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-07-29 16:30:34 +0200

    set up color palette in theme and install font

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
-import { useState } from "react";
 import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material/";
+import "@fontsource/inter";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#FAF9F6",
+      paper: "#F8F8F8",
+    },
+    primary: {
+      light: "#8ECAE6",
+      main: "#219EBC",
+      dark: "#023047",
+    },
+    secondary: {
+      main: "#FFB703",
+      dark: "#FB8500",
+    },
+    text: {
+      primary: "#1D1D1D",
+      secondary: "#8E8E8E",
+    },
+    error: {
+      main: "#d32f2f",
+    },
+    warning: {
+      main: "#FB8500",
+    },
+    success: {
+      main: "#388e3c",
+    },
+    info: {
+      main: "#219EBC",
+    },
+  },
+  typography: {
+    allVariants: {
+      fontFamily: "Inter",
+    },
+  },
+});
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
