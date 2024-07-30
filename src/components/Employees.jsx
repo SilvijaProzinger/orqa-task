8d1ecef799d9835cd2f4d2494176fce0b347f90f
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-07-30 17:49:24 +0200

    call custom hook inside employees component

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,12 +1,37 @@
-import { Box, Typography } from "@mui/material";
+import { useEffect, useRef } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import EmployeesList from "./EmployeesList";
+import EmployeesTable from "./EmployeesTable";
 import Search from "./Search";
+import useFetchData from "../hooks/useFetchData";
+import ErrorMessage from "./ErrorMessage";
+
+const apiUrl = import.meta.env.VITE_API_EMPLOYEES_API ?? "";
 
 const Employees = () => {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
+  const { employeesData, pageData, loading, error, fetchData } = useFetchData(
+    apiUrl,
+    4,
+    ""
+  );
+  const isInitalized = useRef(false);
+
+  useEffect(() => {
+    // add ref to stop useEffect from running twice in a row by default in strict mode (not necessary in production)
+    if (!isInitalized.current) {
+      fetchData();
+      isInitalized.current = true;
+    }
+  }, [fetchData]);
+
+  useEffect(() => {
+    console.log(employeesData);
+    console.log(pageData);
+  }, [employeesData, pageData]);
 
   return (
     <Box
@@ -26,20 +51,37 @@ const Employees = () => {
         </Typography>{" "}
         u tablici te opcijom pretrage pronaći zaposlenike po imenu i prezimenu.
       </Typography>
-      <Box
-        mt={6}
-        mb={3}
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
-        <Typography variant="h4">Popis zaposlenika</Typography>
-        <Search />
-      </Box>
-      <EmployeesList />
+      {error ? (
+        <ErrorMessage error={error} />
+      ) : (
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            mt={6}
+            mb={3}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Typography variant="h4">Popis zaposlenika</Typography>
+            <Search disabled={loading} />
+          </Box>
+          {loading ? (
+            <CircularProgress color="secondary" />
+          ) : (
+            <EmployeesTable employees={employeesData}/>
+          )}
+        </Box>
+      )}
     </Box>
   );
 };
